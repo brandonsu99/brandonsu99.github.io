031b79d5f1ef40221bb374e20ea73c19e4e16669
Brandon Su <45740596+brandonsu99@users.noreply.github.com>
2019-12-01 16:02:33 -0600

    Init Files

---
+++ precache-manifest.4c6c497a221df3445a180d72b60da1c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbeeffd5b46486257b5f7110bd01a72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed63edcc0d8daa5cfaac",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a11ec2499fdc78a6ac21",
+    "url": "/static/css/main.2050d109.chunk.css"
+  },
+  {
+    "revision": "ed63edcc0d8daa5cfaac",
+    "url": "/static/js/2.e7983ab8.chunk.js"
+  },
+  {
+    "revision": "a11ec2499fdc78a6ac21",
+    "url": "/static/js/main.ae9d7a1e.chunk.js"
+  },
+  {
+    "revision": "bab1ba848aa75ad32a0c",
+    "url": "/static/js/runtime-main.ad8868b0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
